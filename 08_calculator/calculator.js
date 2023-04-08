99d5aba8fdf217362f4ea0446c24ccff9ab6f2d1
Nick Davis <nickdavis71@gmail.com>
2023-04-08 02:32:07 -0600

    Completed Exercise 08 Calculator.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, currentValue) => total += currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, currentValue) => total *= currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponential) {
+	return base**exponential;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  factorialValue = 1;
+	for (let i = number; i > 0; i--) {
+    factorialValue *= i;
+  }
+  return factorialValue;
 };
 
 // Do not edit below this line
