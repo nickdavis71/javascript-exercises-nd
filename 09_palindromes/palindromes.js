cc26f8d6193f2984856f90f78c6b19f45f755c7e
Nick Davis <nickdavis71@gmail.com>
2023-04-09 01:28:14 -0600

    Completed JavaScript exercises 9 and 10.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    const reversedString = processedString.split("").reverse().join("");                    
+    return reversedString === processedString;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+if (n < 0) return "OOPS";
+let a = 0;
+let b = 1;
+for ( let i = 1; i < n; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+}
+return b;
 };
+
+console.log(fibonacci(3));
 
 // Do not edit below this line
 module.exports = fibonacci;
