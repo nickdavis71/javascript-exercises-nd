3a77b0b1b93e257fdc17df2ae60ef7a3443b8e76
Nick Davis <nickdavis71@gmail.com>
2023-04-10 23:58:20 -0600

    Completed JavaScript exercises 11 and 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(people) {
+    return people.reduce(function(lastPerson, currentPerson) {
+        const currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        const lastPersonAge = getAge(lastPerson.yearOfBirth, lastPerson.yearOfDeath);
+        return (lastPersonAge > currentPersonAge) ? lastPerson : currentPerson;
+    });
+};
+
+const getAge = function(birthYear, deathYear) {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear;
+    }
+    return deathYear - birthYear;
 };
 
 // Do not edit below this line
